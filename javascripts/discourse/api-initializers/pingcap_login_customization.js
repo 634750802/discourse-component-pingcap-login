fe0d4b30fc2f4525629289b688f9f4da288d60c6
Jagger <634750802@qq.com>
2022-12-23 18:16:03 +0800

    first commit

---
+++ javascripts/discourse/api-initializers/pingcap_login_customization.js
@@ -0,0 +1,48 @@
+import {apiInitializer} from "discourse/lib/api";
+
+export default apiInitializer("0.11.1", api => {
+  console.log(api.settings);
+
+  api.container.lookup("model:login-method").constructor.reopen({
+    // login method is tidb cloud
+    get isCloud() {
+      return this.name === "oauth2_basic";
+    },
+
+    // login method is github
+    get isGithub() {
+      return this.name === "github";
+    },
+
+    get customizedTitle() {
+      switch (this.name) {
+        case 'oauth2_basic':
+          return 'TiDB Cloud';
+        case 'github':
+          return 'Github';
+        case 'google_oauth2':
+          return 'Google';
+        default:
+          return this.name;
+      }
+    },
+  });
+
+  api.container.lookup("component:login-buttons").constructor.reopen({
+    get cloudButton() {
+      return this.buttons.find(button => button.name === 'oauth2_basic');
+    },
+    get googleButton() {
+      return this.buttons.find(button => button.name === 'google_oauth2');
+    },
+    get githubButton() {
+      return this.buttons.find(button => button.name === 'github');
+    },
+    get privacyPolicyUrl() {
+      return Discourse.SiteSettings.privacy_policy_url;
+    },
+    get termOfServiceUrl() {
+      return Discourse.SiteSettings.privacy_policy_url;
+    },
+  });
+});
